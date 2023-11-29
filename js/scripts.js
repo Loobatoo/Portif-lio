41813e07192bc498ee3b8886dbe801b5c66d4888
Pedro Lobato <pedrolfirbida@gmail.com>
2023-11-29 13:21:45 -0300

    att

--- js/scripts.js
+++ js/scripts.js
@@ -51,13 +51,29 @@ window.addEventListener("scroll", (e) => {
   }
 })
 
-ScrollReveal().reveal('.row-1');
+ScrollReveal().reveal('.row-1', {
+  delay: 0,
+  duration: 1000,
+  reset: true
+});
 
-ScrollReveal().reveal('.row-2');
+ScrollReveal().reveal('.row-2', {
+  delay: 0,
+  duration: 1000,
+  reset: true
+});
 
-ScrollReveal().reveal('.row-3');
+ScrollReveal().reveal('.row-3', {
+  delay: 0,
+  duration: 1000,
+  reset: true
+});
 
-ScrollReveal().reveal('.row-4');
+ScrollReveal().reveal('.row-4', {
+  delay: 0,
+  duration: 1000,
+  reset: true
+});
 
 const ImagemPedro = document.querySelector("#IMG01");
 
